b49ac127673a4543d6c47824a1cb0a4349324532
apipops <apollinevast@gmail.com>
2022-11-11 16:06:44 +0100

    correction test

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,26 +6,11 @@
 /*   By: avast <avast@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 18:48:59 by avast             #+#    #+#             */
-/*   Updated: 2022/11/10 17:02:33 by avast            ###   ########.fr       */
+/*   Updated: 2022/11/11 16:06:14 by avast            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-/*
-char	*ft_strrchr(const char *s, int c)
-{
-	char	*p;
-
-	p = 0;
-	while (*s)
-	{
-		if (*s == (char)c)
-			p = (char *)s;
-		s++;
-	}
-	return (p);
-}
-*/
 
 char	*ft_strrchr(const char *s, int c)
 {
