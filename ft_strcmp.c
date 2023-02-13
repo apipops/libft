68228b6e524b037e65e9f8702a74ae6b64b08b75
Apolline VAST <avast@e2r4p12.clusters.42paris.fr>
2023-02-13 16:17:20 +0100

    libft

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -1,28 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: avast <avast@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/10 12:01:39 by avast             #+#    #+#             */
-/*   Updated: 2023/02/02 11:42:42 by avast            ###   ########.fr       */
+/*   Created: 2023/02/10 17:54:25 by avast             #+#    #+#             */
+/*   Updated: 2023/02/10 17:57:58 by avast            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-void	ft_lstadd_back(t_list **lst, t_list *new)
+int	ft_strcmp(const char *s1, const char *s2)
 {
-	t_list	*cur;
+	int	i;
 
-	if (*lst == 0)
-	{
-		*lst = new;
-		return ;
-	}
-	cur = *lst;
-	while (cur->next)
-		cur = cur->next;
-	cur->next = new;
+	i = 0;
+	if (s1 == 0 && s2 == 0)
+		return (0);
+	if (s1 == 0 || s2 == 0)
+		return (-1);
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	if (!s1[i] && !s2[i])
+		return (0);
+	return (s1[i] - s2[i]);
 }
